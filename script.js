778a9ca340cad15b171b88a080a70498089161e6
AshleyZhao <ash_spider@hotmail.com>
2026-04-24 23:45:28 -0400

    Fixed

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// Select the button by its ID
-const button1 = document.getElementById("btn-one");
+// 1. Select ALL buttons with the 'ripple-btn' class
+const buttons = document.querySelectorAll(".custom-button");
 
-// Add a click event listener
-//button.addEventListener('click', () => {
-//    alert('Button was clicked!');
-//    console.log('The 84x84 button is functional.');
-//});
-
-button1.addEventListener('click', function(e) {
-
-  const rect = this.getBoundingClientRect(); // Get exact button position
-  const x = e.clientX - rect.left; 
-  const y = e.clientY - rect.top;
-
-  let ripples = document.createElement('span');
-  ripples.style.left = `${x}px`;
-  ripples.style.top = `${y}px`;
+// 2. Loop through each button in the list
+buttons.forEach(button => {
   
-  this.appendChild(ripples);
+  // 3. Add the event listener to each individual button
+  button.addEventListener('click', function(e) {
+    const rect = this.getBoundingClientRect();
+    const x = e.clientX - rect.left; 
+    const y = e.clientY - rect.top;
 
-  setTimeout(() => {
-    ripples.remove();
-  }, 1000);
+    let ripples = document.createElement('span');
+    ripples.style.left = `${x}px`;
+    ripples.style.top = `${y}px`;
+    
+    this.appendChild(ripples);
+
+    setTimeout(() => {
+      ripples.remove();
+    }, 1000);
+  });
 });
